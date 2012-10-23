ed91e221dbc064f880c47281d59bc0f56a96b5fa
andrew nevins <andrew@red-robot.co.uk>
2012-10-23 11:01:06 +0100

    coursesmodel getCourseByCourseId function working

--- application/models/coursesmodel.php
+++ application/models/coursesmodel.php
@@ -10,32 +10,29 @@ class Coursesmodel extends CI_Model {
 	}
 
 	function getCourseByCourseId( $course_id ) {
-		$courses = new DOMDocument();
-
+		
+		$file = new DOMDocument();
 
 		//load the XML file into the DOM, loading statically
-		$courses->load( dirname(__FILE__) . '/../' . $this->config->item('xml_path') . $this->file );
+		$file->load( dirname(__FILE__) . '/../' . $this->config->item('xml_path') . $this->file );
 
-		$courses->getElementsByTagName('course');
-		foreach ($courses as $course){
+		//get all course nodes
+		$courses = $file->getElementsByTagName('course');
+
+		//set the course attribute of id to type of ID
+		foreach ( $courses as $course ){
 			$course->setIdAttribute( 'id', true);
 		}
 
-
 		//validate the document
-		$courses->validateOnParse = true;
+		$file->validateOnParse = true;
 
-		var_dump($courses->getElementById($course_id));exit;
-
-		$course = $courses->getElementsByTagName("course");
+		//get the course by course id, using the id
+		$course = $file->getElementById( $course_id );
 		
+		return $course;
 
-
-
-
-			return $course;
-
-		}
+	}
 
 
 }
