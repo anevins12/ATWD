211e939a0f475c218e13a630ce0ca38bed81b2d9
andrew nevins <andrew@red-robot.co.uk>
2012-11-09 19:41:37 +0000

    view cleaned up a bit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -65,7 +65,7 @@
 	</style>
 </head>
 <body>
-<a href="index.php/books/">test</a>
+
 <div id="container">
 
 	<?php
@@ -131,23 +131,10 @@
 			</form>
 		</div>
 
+		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 
-
-
-	<h1>Welcome to CodeIgniter!</h1>
-
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
 
 </body>
