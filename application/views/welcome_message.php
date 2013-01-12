e2be14c42476a71e70aee466d835d48aa57c7790
andrew nevins <andrew@red-robot.co.uk>
2013-01-12 19:03:06 +0000

    cleaned up some of the JSON code

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -198,12 +198,23 @@ if ( isset( $json ) && $json ) {
 				
 				//suggestions output
 				if ( results.suggestions ) {
-					$.each(results.suggestions, function(){
 
+					//that can be looped over
+					if ( results.suggestions.length > 1 ) {
+						$.each(results.suggestions, function(){
+
+							var isbn = "<h4> Isbn: " + this.isbn + "</h4>";
+							$("#results ul").append("<li>" + isbn + "</li>");
+
+						});
+					}
+					//that can't be looped over
+					else { 
 						var isbn = "<h4> Isbn: " + this.isbn + "</h4>";
 						$("#results ul").append("<li>" + isbn + "</li>");
 
-					});
+					}
+
 				}
 
 				//for all other retrieval of information, that can be looped over
@@ -219,14 +230,17 @@ if ( isset( $json ) && $json ) {
 					});
 				}
 
-				//for all other retrieval of informaiton, that can't be looped over'
+				//can't be looped over
 				else {
+					
 					var title          = "<h3>" + results.title + "</h3>";
 					var isbn           = "<h4> Isbn: " + results.isbn + "</h4>";
 					var borrowed_count = "<h4> Borrowed count: " + results.borrowedcount + "</h4>";
 
 					$("#results ul").append("<li><hgroup>" + title + "\n" + isbn + "\n" + borrowed_count + "</hgroup></li>");
+				
 				}
+
 
 			});
 			
